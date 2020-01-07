37f1538382fe93b757ce724a554718c570351ca0
sabing123 <sabingautam05@gmail.com>
2020-01-07 10:23:32 +0545

    new files added

--- app/src/main/java/com/sabin/foodmandu/ui/home/HomeFragment.java
+++ app/src/main/java/com/sabin/foodmandu/ui/home/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -13,23 +15,33 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.sabin.foodmandu.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
+import com.synnapps.carouselview.ImageListener;
 
 public class HomeFragment extends Fragment {
+    CarouselView carouselView;
 
-    private HomeViewModel homeViewModel;
+    int[] sampleImages = {R.drawable.ad1, R.drawable.ad2};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+      View view = inflater.inflate(R.layout.fragment_home, container,false);
+      carouselView = view.findViewById(R.id.carouselView);
+      carouselView.setPageCount(sampleImages.length);
+      carouselView.setImageListener(new ImageListener() {
+          @Override
+          public void setImageForPosition(int position, ImageView imageView) {
+              imageView.setImageResource(sampleImages[position]);
+          }
+      });
+
+      carouselView.setImageClickListener(new ImageClickListener() {
+          @Override
+          public void onClick(int position) {
+              Toast.makeText(getContext(), sampleImages[position], Toast.LENGTH_SHORT).show();
+          }
+      });
+      return view;
     }
 }
