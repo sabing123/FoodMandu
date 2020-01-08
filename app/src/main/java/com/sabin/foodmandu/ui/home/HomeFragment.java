fba5343c10b43420af83570d728e4344cc8aab2a
sabing123 <sabingautam05@gmail.com>
2020-01-08 09:28:01 +0545

    App bar Change

--- app/src/main/java/com/sabin/foodmandu/ui/home/HomeFragment.java
+++ app/src/main/java/com/sabin/foodmandu/ui/home/HomeFragment.java
@@ -16,32 +16,97 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.sabin.foodmandu.URL.url;
 import com.sabin.foodmandu.adapter.categoriesAdapter;
 import com.sabin.foodmandu.R;
+import com.sabin.foodmandu.api.super7API;
 import com.sabin.foodmandu.model.Categories;
+import com.sabin.foodmandu.model.super7;
+import com.squareup.picasso.Picasso;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static android.graphics.Color.RED;
+
 public class HomeFragment extends Fragment {
     CarouselView carouselView;
+    private TextView tvSuper7;
 private RecyclerView categoryRecycleView;
     int[] sampleImages = {R.drawable.ad1, R.drawable.ad2};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
       View view = inflater.inflate(R.layout.fragment_home, container,false);
+     //advertisement
       carouselView = view.findViewById(R.id.carouselView);
+        imageSlider();
+
+        //category
       categoryRecycleView = view.findViewById(R.id.CategoryrecyclerView);
-      imageSlider();
-      CategoryRecycle();
+        CategoryRecycle();
 
-
+        //super7
+      tvSuper7 = view.findViewById(R.id.tvsuper7);
+      super7();
 
       return view;
+    }
+
+    private void super7() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:3000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        super7API super7API = retrofit.create(super7API.class);
+
+        Call<List<super7>> ListCall = super7API.getSuper7();
+        super7API super7API1 = url.getInstance().create(super7API.class);
+
+        ListCall.enqueue(new Callback<List<super7>>() {
+
+            @Override
+            public void onResponse(Call<List<super7>> call, Response<List<super7>> response) {
+                if(!response.isSuccessful()){
+                    tvSuper7.setText(" Code : " + response.code());
+                    return;
+                }
+                List<super7> super7List = response.body();
+
+                for (super7 super7 : super7List ){
+                    String content = " ";
+                    content +=  super7.getImage() + "\n";
+                    content +=  (super7.getName()+ "\n") ;
+                    content +=  super7.getLocation()+ "\n";
+
+                    tvSuper7.append(content);
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<super7>> call, Throwable t) {
+    tvSuper7.setText("Error" + t.getMessage());
+            }
+        });
+
+
+
+
     }
 
     private void CategoryRecycle() {
